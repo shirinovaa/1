115f992d35025de025bb32ca2c86d76e8271cbc4
shirinovaa <shirinovafatima8@gmail.com>
2022-01-07 16:34:39 +0600

    tedt

--- src/com/company/Boss.java
+++ src/com/company/Boss.java
@@ -1,2 +1,31 @@
-package com.company;public class Boss {
+package com.company;
+
+public class Boss {
+    private int health ;
+    private int damage;
+    private String superPower;
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+
+    public String getSuperPower() {
+        return superPower;
+    }
+
+    public void setSuperPower(String superPower) {
+        this.superPower = superPower;
+    }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,29 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Boss boss = new Boss();
+        boss.setDamage(15);
+        boss.setHealth(860);
+        boss.setSuperPower("SuperPower");
+        System.out.println(boss.getDamage());
+        System.out.println(boss.getHealth());
+        System.out.println(boss.getSuperPower());
+        Hero[] heroes = createHeroes();
+
+        for (int i = 0; i < heroes.length ; i++){
+            System.out.println(heroes[i].getDamage()+ heroes [1].getHealth()+heroes[1].getSuperPower());
+            System.out.println("String" +10);
+        }
+
+
     }
+
+    public static Hero[] createHeroes() {
+        Hero magic = new Hero(170,20,"darkness");
+        Hero physical = new Hero(200,15);
+        Hero dragon = new Hero(270,20);
+        return new Hero[] {magic,physical,dragon};
+    }
+
+
 }
